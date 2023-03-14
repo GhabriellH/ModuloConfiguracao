caa73904b43ec3c2db3a4025e24600d21e33b59d
Ghabriell <ghabriellhenrick15@gmail.com>
2023-03-14 16:49:37 -0300

    Criação CRUD
    Criando os métodos CRUD(criação, leitura e delete).

---
+++ Configuracao/DAL/GrupoUsuarioDAL.cs
@@ -0,0 +1,35 @@
+﻿using Models;
+using System.Collections.Generic;
+using System;
+
+namespace DAL
+{
+    public class GrupoUsuarioDAL
+    {
+        public void Iserir(GrupoUsuario _grupoUsuario)
+        {
+
+        }
+        public List<GrupoUsuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public List<GrupoUsuario> BuscarPorNomeGrupo(string _nomeGrupo)
+        {
+            throw new NotImplementedException();
+        }
+        public List<GrupoUsuario> BuscarPorId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar(GrupoUsuario _grupoUsuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+    }
+}
---
+++ Configuracao/DAL/PermissaoDAL.cs
@@ -0,0 +1,35 @@
+﻿using Models;
+using System.Collections.Generic;
+using System;
+
+namespace DAL
+{
+    internal class PermissaoDAL
+    {
+        public void Iserir(Permissao _permissao)
+        {
+
+        }
+        public List<Permissao> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public List<Permissao> BuscarPorDescricao(string _descricao)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Permissao> BuscarPorId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar(Permissao _permissao)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+    }
+}
